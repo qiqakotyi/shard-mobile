6e8543d79d4331f60dc4231784e66c52f7618e7a
Qiqa Kotyi <Qiqa.Kotyi@Digioutsource.com>
2017-12-04 10:54:09 +0200

    created wrapped app

---
+++ src/pages/home/home.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { TouchID } from '@ionic-native/touch-id';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private touchId: TouchID) {
+    
+    this.touchId.isAvailable()
+    .then(
+      res => console.log('TouchID is available!'),
+      err => console.error('TouchID is not available', err)
+    );
+  
+    this.touchId.verifyFingerprint('Scan your fingerprint please')
+    .then(
+      res => console.log('Ok', res),
+      err => console.error('Error', err)
+    );
+  }
+
+  ngOnInit() {
+
+    const browser = this.iab.create('https://www.hippodromeonline.com/', '_self', { location: 'no' });
+
+    console.log(browser);
+  }
+} 
